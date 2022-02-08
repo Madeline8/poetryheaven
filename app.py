f2cb8e8042959b7d18a46f2821373c8e51b79420
Magda <madeline@gmx.co.uk>
2022-02-08 22:32:37 +0000

    Fixed connection for poems.html. Edits on home.html

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-"""
-"""
-
 import os
 from flask import (
     Flask, flash, render_template,
@@ -31,10 +28,20 @@ mongo = PyMongo(app)
 
 # Home Page
 @app.route("/")
-@app.route("/get_poems")
-def get_poems():
-    poems = mongo.db.poems.find()
-    return render_template("poems.html", poems=poems)
+@app.route("/home")
+def home():
+    poems=list(mongo.db.poems.find())
+    mobile_poems=[poems[0], poems[2], poems[5]]
+    return render_template(
+        "home.html", poems=poems, mobile_poems=mobile_poems)
+
+
+@app.route("/poems")
+def poems():
+    poems=mongo.db.poems.find()
+    return render_template(
+        "poems.html", poems=poems)
+
 
 # Sign Up Page
 @app.route("/signup", methods=["GET", "POST"])
@@ -75,11 +82,11 @@ def login():
         if existing_user:
             # ensure hashed password matches user input
             if check_password_hash(
-                    existing_user["password"], request.form.get("password")):
-                        session["user"] = request.form.get("username").lower()
-                        flash("Hi, {}".format(
-                            request.form.get("username")))
-                        return redirect(url_for("profile", username=session["user"]))
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Hi, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for("profile", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect Username and/or Password")
